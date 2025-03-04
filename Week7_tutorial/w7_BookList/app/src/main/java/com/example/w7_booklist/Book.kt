b0700bf797a79aa2f944dd10d5a55b53b7c90314
ducanh <ducanh15022004@gmail.com>
2025-03-04 22:33:14 +0700

    Add Week7_tutorial folder to COS30017

---
+++ Week7_tutorial/w7_BookList/app/src/main/java/com/example/w7_booklist/Book.kt
@@ -0,0 +1,3 @@
+package com.example.w7_booklist
+
+data class Book(val title: String, val rating: Float, val image: String)
---
+++ Week7_tutorial/w7_BookList/app/src/main/java/com/example/w7_booklist/BookStore.kt
@@ -0,0 +1,14 @@
+package com.example.w7_booklist
+
+
+object BookStore {
+
+    val books = listOf(
+        Book("Mobilities", 4.0F, "2581744"),
+        Book("Pascal", 1.0F, "9999512"),
+        Book("The Hitchhiker's Guide to the Galaxy", 5.0F, "8594912"),
+        Book("Concepts of modern art", 2.0F,"316050")
+    )
+
+    val count = books.size
+}
